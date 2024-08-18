f1e2aa25bc813377e2bdb942bf0674fb196c55c8
usamamaan <usamamaan37@gmail.com>
2024-08-18 12:41:48 +0500

    adding material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,11 @@ import { AboutComponent } from './about/about.component';
 import { HelpComponent } from './help/help.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ReportComponent } from './report/report.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +20,19 @@ import { ReportComponent } from './report/report.component';
     AboutComponent,
     HelpComponent,
     RegistrationComponent,
-    ReportComponent
+    ReportComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  ComplainType: any[] = ['Service Problem', 'connectivity problem ', 'other'];
+  selectedComplain: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
